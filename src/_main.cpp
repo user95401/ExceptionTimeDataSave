17e45268bc7c8ca06cfd756c17eedf5c79762e7f
user95401 <user95401@gmail.com>
2023-11-27 19:42:33 +0400

    1.1.0 - now really works
    yea
    saves level on crash
    saves game on crash

--- src/_main.cpp
+++ src/_main.cpp
@@ -5,24 +5,54 @@ using namespace cocos2d;
 using namespace cocos2d::extension;
 using namespace gd;
 
+LONG WINAPI VectoredExceptionHandler(_EXCEPTION_POINTERS* pExceptInfo) {
+    //save game?
+    GameManager::sharedState()->save();
+    ModUtils::log("game saved");//save log why no
+    //level editor tries
+    LevelEditorLayer* LevelEditorLayer_ = GameManager::sharedState()->getEditorLayer();
+    if (LevelEditorLayer_) {
+        LevelEditorLayer_->getEditorUI()->undoLastAction(CCNode::create());
+        LevelEditorLayer_->getEditorUI()->undoLastAction(CCNode::create());
+        LevelEditorLayer_->getEditorUI()->undoLastAction(CCNode::create());
+        ModUtils::log("was performed 3 undo actions in current level editor layer");
+        EditorPauseLayer::create(LevelEditorLayer_)->saveLevel();
+        ModUtils::log("current level saved");
+    }
+    //error msg
+    DWORD exc_code = pExceptInfo->ExceptionRecord->ExceptionCode;
+    bool dontCare = false;
+    //msg text
+    std::stringstream buffer;
+    buffer << "Received fatal exception: ";
+    switch (exc_code) {
+    case DBG_PRINTEXCEPTION_WIDE_C:
+        buffer << "DBG_PRINTEXCEPTION_WIDE_C";
+        dontCare = true;
+        break;
+    case DBG_PRINTEXCEPTION_C:
+        buffer << "DBG_PRINTEXCEPTION_C";
+        dontCare = true;
+        break;
+    default:
+        buffer << "0x" << std::hex << exc_code;
+        break;
+    }
+    ModUtils::log(buffer.str());
+    //msg text makeup end
+    if (dontCare) return EXCEPTION_CONTINUE_EXECUTION;//came from 2.2 pc gdps... idk if needs here
+    return EXCEPTION_CONTINUE_SEARCH;
+}
 
 DWORD WINAPI PROCESS_ATTACH(void* hModule) {
     //nothing here yet...
     return 0;
 }
 
-LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS* info) {
-    //save game?
-    GameManager::sharedState()->save();
-    //who is it also ok
-    AppDelegate::sharedApplication()->trySaveGame();
-    return EXCEPTION_CONTINUE_SEARCH;
-}
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         CreateThread(0, 0, PROCESS_ATTACH, hModule, 0, 0);
-        SetUnhandledExceptionFilter(ExceptionHandler);
+        AddVectoredExceptionHandler(1, VectoredExceptionHandler);
     }
     return TRUE;
 }
